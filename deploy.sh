051c4095f3162248b2d41becd6d45fe7bff347ac
Jason Wu <jason.s.j.wu@oocl.com>
2018-05-29 17:11:40 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ scp -i $keyfile ./build/libs/demo-*.jar ubuntu@$WebServer:/home/ubuntu/works/$Na
 ssh -i $keyfile ubuntu@$WebServer ls /home/ubuntu/works/$NameSpace
 ssh -i $keyfile ubuntu@$WebServer 'kill -9 $(lsof -t -i:'$port') 2>&1 &'
 ssh -i $keyfile ubuntu@$WebServer 'nohup java -jar /home/ubuntu/works/'$NameSpace'/demo.jar > /home/ubuntu/works/'$NameSpace'/demo.log 2>&1 &'
-ssh -i $keyfile ubuntu@$WebServer 'ping '$WebServer':'$port''
+ssh -i $keyfile ubuntu@$WebServer 'ping '@$WebServer':'$port''
